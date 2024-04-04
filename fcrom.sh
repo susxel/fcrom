60db0d8268da0f3dfc3f1ecfbf199f94cecb1f36
Camila Baruĥ <cbaruch@protonmail.com>
2024-04-04 12:28:08 +0200

    Moved .INF files to home config dir

--- fcrom.sh
+++ fcrom.sh
@@ -9,14 +9,18 @@ if [ `hexdump "$filename" -n 4 -e '"%x"'` != "1a53454e" ]; then
 	zenity --error --text="Not an .NES file"
 	exit
 else
+	basename=`basename "$filename"`
 	prg=$((`hexdump "$filename" -s 4 -n 1 -e '"%i"'`))
 	chr=$((`hexdump "$filename" -s 5 -n 1 -e '"%i"'`))
 	lmap=`hexdump "$filename" -s 6 -n 1 -e '"%x"'`
 	hmap=`hexdump "$filename" -s 7 -n 1 -e '"%x"'`
-	echo "PRG ROM: $(($prg*16)) KB ($(($prg*16*8)) Kb)" > "$filename.inf"
-	echo "CHR ROM: $(($chr*8)) KB ($(($chr*8*8)) Kb)" >> "$filename.inf"
-	echo "Mapper: `printf "%03.0f" $((16#${hmap:0:1}${lmap:0:1}))`" >> "$filename.inf"
-	zenity --text-info --title="Header info" --filename="$filename.inf" --ok-label="Show mapper info" --cancel-label="Do not show mapper info"
+	cd ~
+	mkdir .config/fcrom	
+	mkdir .config/fcrom/inf
+	echo "PRG ROM: $(($prg*16)) KB ($(($prg*16*8)) Kb)" > ".config/fcrom/inf/$basename.inf"
+	echo "CHR ROM: $(($chr*8)) KB ($(($chr*8*8)) Kb)" >> ".config/fcrom/inf/$basename.inf"
+	echo "Mapper: `printf "%03.0f" $((16#${hmap:0:1}${lmap:0:1}))`" >> ".config/fcrom/inf/$basename.inf"
+	zenity --text-info --title="Header info" --filename=".config/fcrom/inf/$basename.inf" --ok-label="Show mapper info" --cancel-label="Do not show mapper info"
 	case $? in
 		0)
 			if [ ${hmap:0:1} ]; then
